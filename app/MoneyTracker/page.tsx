a7e082838beee19f8643ee12b6fad23123cdd8cb
zonghao04 <zonghao04@gmail.com>
2024-11-17 05:36:31 +0800

    1

--- app/MoneyTracker/page.tsx
+++ app/MoneyTracker/page.tsx
@@ -117,62 +117,7 @@ export default function Home() {
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-20">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-slate-200 ring-1 ring-slate-500 hover:ring-white">
-              Devcon 2024 LFGGGGG.{' '}
-              <a href="https://devcon.org/en/" className="font-semibold text-indigo-300">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
-          <div className="text-center">
-            <img
-                alt="Company Logo"
-                src="/logo_white.png"
-                className="mx-auto h-15 w-auto"
-            />
-            <h1 className="text-balance text-5xl font-semibold tracking-tight text-slate-200 sm:text-7xl">
-              Kaching 2.0
-            </h1>
-            <p className="mt-8 text-pretty text-lg font-medium text-gray-400 sm:text-xl/8">
-            A decentralized tool that helps you keep track of your crypto transactions. It’s built for privacy and transparency, giving you control over your financial data while making it easy to organize and review your activity in the Web3 world.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="text-sm/6 font-semibold text-slate-200 hover:underline">
-                Get started
-              </a>
-              <a href="#" className="text-sm/6 font-semibold text-slate-200">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}/>
-        </div>
+        
       </div>
     </div>
   )
